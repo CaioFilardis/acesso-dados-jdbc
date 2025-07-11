dd38cdeeea15ed0c3ea12dd141fdce422fd10993
Caio Filardis <caiosantosshake@hotmail.com>
2025-07-10 22:39:26 -0300

    Implementando uma nova classe de execução para mostrar como inserir dados no banco de dados, utilizando a classe 'PreparedStatement', a partir da função '.executeUpdate' e scripts SQL. Ou seja, realiazar a inserção de dados simples com 'preparedStatement' e inserção de com recuperação de ID.

--- src/applicaiton/Program.java
+++ src/applicaiton/Program.java
@@ -1,9 +1,12 @@
 package applicaiton;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import br.com.db.DB;
 
@@ -11,33 +14,58 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null; // faz a conexão com o banco
-		Statement st = null; // faz a consulta com o banco
-		ResultSet rs = null; // recupera os dados após a consulta em forma de tabela
+		// formatando uma data especificamente
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		// recuperar dados do banco
+		// inserir um novo vendedor
+		Connection conn = null; 
+		PreparedStatement st = null;
 		try {
-			conn = DB.getConnection(); // conecta com o banco de dados
+			// realizar conexão
+			conn= DB.getConnection(); 
 			
-			st = conn.createStatement(); // instanciando objeto statement
+			// inserindo dados utilizando scripts SQL, instanciando o 'PreparedStatement'
+			st = conn.prepareStatement(
+					"INSERT INTO seller " // inserir dados
+					+ "(name, Email, BirthDate, BaseSalary, DepartmentId)" // selecionando os parâmetros
+					+ "VALUES " // inserir os valores abaixo
+					+ "(?, ?, ?, ?, ?)",
+					+ Statement.RETURN_GENERATED_KEYS); // recuperação de Id 
+					
+			// definindo os valores dos campos, com base np seu tipo de dado definido na tabela
+			st.setString(1, "Carl Purple"); 
+			st.setString(2, "carl@gmail.com"); // ?, valor-selecionado
+			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime())); // instanciar uma data no JDBC
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
 			
-			rs = st.executeQuery("SELECT * FROM coursejdbc.department"); // recupera os dadoas da tabela 'departament'
+			// executar o comando, definindo às linhas alteradas
+			int rowsAffected = st.executeUpdate();
 			
-			// 'next()', funcao que percorre a tabela do banco
-			while (rs.next()) { 
-				// imprime as linhas da coluna
-				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			if (rowsAffected > 0) {
+				// pegar o código do novo registro inserido
+				ResultSet rs =st.getGeneratedKeys();
+				
+				// pode gerar mais de um valor
+				while(rs.next()) {
+					int id = rs.getInt(1); // quero o valor da primeira coluna
+					System.out.println("Done! Id = " + id);
+				}
+			}
+			else {
+				System.out.println("No rown affected"); // nenhuma linha alterada
 			}
 		}
-		catch (SQLException e) {
-			e.printStackTrace(); // exceção aponta o erro
+		catch(SQLException e) {
+			e.printStackTrace();
 		}
-		// aplicando boas práticas(evitar o vazamento de memória, isso é necessário devido às funções usadas)
+		catch(ParseException e) { // vai tratar os erros de data em formato inválido
+			e.printStackTrace();
+		}
 		finally {
-			DB.closeStatement(st);
-			DB.closeResultSet(rs);
-			DB.closeConnection(conn);
+			// fechar os recurso que foram abertos
+			DB.closeStatement(st); // fechar a função de inserção dos scripts SQL
+			DB.closeConnection(conn); // fechar a função de conexão com p banco
 		}
 	}
-
 }
---
+++ src/applicaiton/Program2.java
@@ -0,0 +1,43 @@
+package applicaiton;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.com.db.DB;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Connection conn = null; // faz a conexão com o banco
+		Statement st = null; // faz a consulta com o banco
+		ResultSet rs = null; // recupera os dados após a consulta em forma de tabela
+		
+		// recuperar dados do banco
+		try {
+			conn = DB.getConnection(); // conecta com o banco de dados
+			
+			st = conn.createStatement(); // instanciando objeto statement
+			
+			rs = st.executeQuery("SELECT * FROM coursejdbc.department"); // recupera os dadoas da tabela 'departament'
+			
+			// 'next()', funcao que percorre a tabela do banco
+			while (rs.next()) { 
+				// imprime as linhas da coluna
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace(); // exceção aponta o erro
+		}
+		// aplicando boas práticas(evitar o vazamento de memória, isso é necessário devido às funções usadas)
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection(conn);
+		}
+	}
+
+}
